8660f7246d1fd2945bfe68ba48dee5f04e0afa95
Vedant Rawat <vedant.rawat@bacancy.com>
2023-08-02 11:14:33 +0530

    variable added

--- Kalkuleight.js
+++ Kalkuleight.js
@@ -1,4 +1,4 @@
-function kalkuleight(principal, time, rate) {
+function kalkuleight(principal, rate, time, frequency) {
   rate = rate / 100;
 
   const amount = principal * Math.pow(1 + rate / frequency, frequency * time);
